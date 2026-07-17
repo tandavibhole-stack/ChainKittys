289b83c8da6274f12b21fd7e25c99986090f5410
ChainKitty Dev <dev@chainkitty.io>
2026-07-17 16:25:56 +0530

    contract skeleton

---
+++ contracts/chainkitty/src/lib.rs
@@ -0,0 +1,12 @@
+﻿#![no_std]
+use soroban_sdk::{contract, contractimpl, contracttype, symbol_short, Address, Env, Vec, Symbol};
+
+#[contract]
+pub struct ChainKittyContract;
+
+#[contractimpl]
+impl ChainKittyContract {
+    pub fn initialize(env: Env, admin: Address, token: Address) -> Result<(), u32> {
+        Ok(())
+    }
+}
